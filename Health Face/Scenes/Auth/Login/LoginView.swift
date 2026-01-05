ef19632e1cdc5ccd3c1820fb75c7ac01f4c5e378
Damir Aushenov <yespeace@bk.ru>
2026-01-05 23:50:08 +0600

    fx

--- Health Face/Scenes/Auth/Login/LoginView.swift
+++ Health Face/Scenes/Auth/Login/LoginView.swift
@@ -10,14 +10,43 @@ import SwiftUI
 
 struct LoginView: View {
     @StateObject private var viewModel: LoginViewModel
-       
-       init(viewModel: LoginViewModel) {
-           _viewModel = StateObject(wrappedValue: viewModel)
-       }
+    @State private var username = ""
     
-    var body: some View {
-        Text("Hello")
+    init(viewModel: LoginViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
     }
     
-    
+    var body: some View {
+        VStack {
+            Text("Welcome back!")
+            TextField(LocalizedStringKey("LoginView.username.text"), text: $username)
+                .padding(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 7)
+                        .stroke(.secondary.opacity(0.5), lineWidth: 0.5))
+            TextField(LocalizedStringKey("LoginView.password.text"), text: $username)
+                .padding(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 7)
+                        .stroke(.secondary.opacity(0.5), lineWidth: 0.5))
+            
+            Button {
+                
+            } label: {
+                Text(LocalizedStringKey("LoginView.login.text"))
+                    .font(.headline)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+                    .padding(.horizontal)
+            }
+            
+            
+            
+        }
+        .padding(14)
+        
+    }
 }
