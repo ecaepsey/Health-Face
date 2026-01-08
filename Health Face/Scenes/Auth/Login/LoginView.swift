0afe630bc9f0b6e26d18bffc3727f17bfe9ea70d
Damir Aushenov <yespeace@bk.ru>
2026-01-08 20:56:40 +0600

    added image

--- Health Face/Scenes/Auth/Login/LoginView.swift
+++ Health Face/Scenes/Auth/Login/LoginView.swift
@@ -17,52 +17,70 @@ struct LoginView: View {
     }
     
     var body: some View {
-        VStack {
-            Text("Welcome back!")
-                .bold()
-                .font(.title)
-                .padding(.horizontal)
-            TextField(LocalizedStringKey("LoginView.username.text"), text: username)
-                .padding(12)
-                   .background(
-                       RoundedRectangle(cornerRadius: 10)
-                           .fill(Color(.systemGray6))
-                           .overlay(
-                               RoundedRectangle(cornerRadius: 10)
-                                   .stroke(isFocused ? Color.blue : Color.clear, lineWidth: 1)
-                           )
-                   )
-            
+        ZStack {
+            VStack {
+                Image("dashboardBg")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 240)      // 200–300 px
+                    
+                    .clipped()
                 
-            TextField(LocalizedStringKey("LoginView.password.text"), text: password)
-                .padding(12)
-                   .background(
-                       RoundedRectangle(cornerRadius: 10)
-                           .fill(Color(.systemGray6))
-                           .overlay(
-                               RoundedRectangle(cornerRadius: 10)
-                                   .stroke(isFocused ? Color.blue : Color.clear, lineWidth: 1)
-                           )
-                   )
-               
-            
-            Button {
-                viewModel.handle(.logInTapped)
-            } label: {
-                Text(LocalizedStringKey("LoginView.login.text"))
-                    .font(.headline)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.black)
-                    .foregroundColor(.white)
-                    .cornerRadius(12)
-                    .padding(.horizontal)
+                Spacer()
             }
-            
-            
-            
+            VStack(spacing: 16) {
+                
+                Text("Welcome back!")
+                    .bold()
+                    .font(.title)
+                    .padding(.horizontal)
+                TextField(LocalizedStringKey("LoginView.username.text"), text: username)
+                    .padding(12)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color(.systemGray6))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(isFocused ? Color.blue : Color.clear, lineWidth: 1)
+                            )
+                    )
+                
+                
+                TextField(LocalizedStringKey("LoginView.password.text"), text: password)
+                    .padding(12)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color(.systemGray6))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(isFocused ? Color.blue : Color.clear, lineWidth: 1)
+                            )
+                    )
+                
+                
+                
+                Button {
+                    viewModel.handle(.logInTapped)
+                } label: {
+                    Text(LocalizedStringKey("LoginView.login.text"))
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                        .padding(.horizontal)
+                }
+                
+                
+                
+                
+            }
+            .frame(maxWidth: 360)
+            .padding(.horizontal, 24)
+           
         }
-        .padding(14)
+        .ignoresSafeArea(edges: .top)
         
     }
     
